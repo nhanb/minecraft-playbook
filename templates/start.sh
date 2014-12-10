6d51edc725b9bb1072ec53e0648c534664f4c182
nhanb <nhan@nerdyweekly.com>
2014-12-10 10:42:58 +0700

    strip tmux logic out of start.sh script
    Because spigot server will try running this script when restarting.

--- templates/start.sh
+++ templates/start.sh
@@ -1,10 +1,3 @@
 #!/usr/bin/env bash
 
-PID=$(pidof java)
-
-if [ -z "$PID" ]; then
-  tmux new-session -d -s prod -n 0 "java -Xms{{ heap_size }} -Xmx{{ heap_size }} -XX:MaxPermSize=128M -jar {{ prod_dir }}/spigot.jar";
-else
-  tmux attach-session -d -t prod ;
-  tmux select-window -t 0 ;
-fi
+java -Xms{{ heap_size }} -Xmx{{ heap_size }} -XX:MaxPermSize=128M -jar {{ prod_dir }}/spigot.jar
